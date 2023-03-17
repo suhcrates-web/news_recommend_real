cd4d84a2f197da66ef8787b4e0effd7518550dd3
suhcrates <suhcrates@hanmail.net>
2023-03-17 15:21:05 +0900

    asdf

--- exp29.py
+++ exp29.py
@@ -10,30 +10,30 @@ print(cwd)   # working directory
 print(os.path.abspath(__file__))  # running file
 
 
-
-service_name = ['db0_realtime_update', 'db2_realtime_update', 'db_mysql_daily_update', 'news_recommend']
-
-working_dir =os.getcwd()
-for name0 in service_name:
-    service_config =f"""
-[Unit]
-Description= {name0}
-
-[Service]  
-User=donga
-WorkingDirectory={working_dir}
-Environment="PATH={sys.prefix}"
-ExectStart={sys.executable} {working_dir}/{name0}.py
-Restart=always
-
-[Install] 
-WantedBy=multi-user.target 
-"""
-
-    service_file = f"/etc/systemd/systme/{name0}.service"
-    with open(service_file, "w") as f:
-        f.write(service_config)
-
-    os.system("sudo systemctl daemon-reload")
-    os.system(f"sudo systemctl start {name0}.service")
-    os.system(f"sudo systemctl enable {name0}.service")
+#
+# service_name = ['db0_realtime_update', 'db2_realtime_update', 'db_mysql_daily_update', 'news_recommend']
+#
+# working_dir =os.getcwd()
+# for name0 in service_name:
+#     service_config =f"""
+# [Unit]
+# Description= {name0}
+#
+# [Service]
+# User=donga
+# WorkingDirectory={working_dir}
+# Environment="PATH={sys.prefix}"
+# ExectStart={sys.executable} {working_dir}/{name0}.py
+# Restart=always
+#
+# [Install]
+# WantedBy=multi-user.target
+# """
+#
+#     service_file = f"/etc/systemd/systme/{name0}.service"
+#     with open(service_file, "w") as f:
+#         f.write(service_config)
+#
+#     os.system("sudo systemctl daemon-reload")
+#     os.system(f"sudo systemctl start {name0}.service")
+#     os.system(f"sudo systemctl enable {name0}.service")
---
+++ test_install.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+working_dir =os.getcwd()
+name0='test0'
+service_config = f"""[Unit]
+Description= test
+
+[Service]  
+User=donga
+WorkingDirectory={working_dir}
+Environment="PATH={sys.prefix}/bin"
+ExectStart={sys.executable} {working_dir}/{name0}.py
+Restart=always
+
+[Install] 
+WantedBy=multi-user.target 
+""".replace('\\', '/')
+
+service_file = f"/etc/systemd/systme/{name0}.service"
+with open(service_file, "w") as f:
+    f.write(service_config)
+
+os.system("sudo systemctl daemon-reload")
+os.system(f"sudo systemctl start {name0}.service")
+os.system(f"sudo systemctl enable {name0}.service")
