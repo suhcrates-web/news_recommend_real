46413e3de9f5479d10b192164099ac9d94c5340c
suhcrates <suhcrates@hanmail.net>
2023-03-19 00:32:05 +0900

    asdf

--- db2_realtime_update.py
+++ db2_realtime_update.py
@@ -7,13 +7,16 @@ import mysql.connector
 from database import config
 import time
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 
 pool = ConnectionPool(host='localhost', port=6379, db=0)
 pool2 = ConnectionPool(host='localhost', port=6379, db=2)
+pool3 = ConnectionPool(host='localhost', port=6379, db=3)
 
 r = redis.Redis(connection_pool=pool)
 r2 = redis.Redis(connection_pool=pool2)
+r3 = redis.Redis(connection_pool=pool3)
+
 def db2_updater():
     db = mysql.connector.connect(**config)
     cursor = db.cursor()
@@ -60,11 +63,26 @@ def db2_updater():
     r2.set('thumburl', json.dumps(thumburl_list))
     r2.set('temp', np.random.rand(50).astype('float32').tobytes()) # ***** 수정해야함
 
+def db3_updater():
+    ##### db3 update 도 같이 해줌. 60분이 지난 r3 키를 지워주는 역할.
+    r3key = r3.keys('*')
+    for time0 in r3key:
+        if datetime.strptime(time0.decode('utf-8') + '0', "%Y%m%d%H:%M") < datetime.now() - timedelta(hours=1):
+            r3.delete(time0)
+    return len(r3key)
+
 if __name__ == '__main__':
     time.sleep(60)
+    n=0
     while True:
+        n+=1
         now0 = datetime.now()
         db2_updater()
-        print(f"업데이트 : {now0}")
+        if n%3 ==0:
+            r3key = db3_updater()
+            n=0
+            print(f"업데이트 : {now0} / r3 key 수 : {r3key}") # 6개 내외로 유지돼야 함.
+        else:
+            print(f"업데이트 : {now0}")
         sys.stdout.flush()
         time.sleep(60*4)
--- main.py
+++ main.py
@@ -1,48 +1,35 @@
 from fastapi import FastAPI
 import numpy as np
-import timeit
 import json
 import redis
-from redis import Redis, ConnectionPool
+from redis import ConnectionPool
 import uvicorn
-from pydantic import BaseModel
-from database import config
-
+from datetime import datetime
 
 def find_10_alt(tot_mat, user_vector):
     points = np.matmul(tot_mat, user_vector)
     top10 = np.argsort(points)[::-1][:10]
     return top10
-#
-# class item0(BaseModel):
-#     ga: str
-#     gid: str =None
-#     num0: int =10
-
 
 app = FastAPI()
 
 pool = ConnectionPool(host='localhost', port=6379, db=0)
 pool1 = ConnectionPool(host='localhost', port=6379, db=1)
 pool2 = ConnectionPool(host='localhost', port=6379, db=2)
+pool3 = ConnectionPool(host='localhost', port=6379, db=3)
 r = redis.Redis(connection_pool=pool)
 r1 = redis.Redis(connection_pool=pool1)
 r2 = redis.Redis(connection_pool=pool2)
+r3 = redis.Redis(connection_pool=pool3)
 
 @app.get("/{ga}/{gid}")
 async def hello(ga:str, gid:str=None):
     gid = None if gid=='_' else gid
+    r3.rpush(datetime.now().strftime('%Y%m%d%H:%M')[:-1],f'["{ga}","{gid}"]')
     p = 0.3
-    time0 = []
-    time1 = []
-    result0 = []
-    result1 = []
-    # time.sleep(3)
-    start = timeit.default_timer()
-    #######
     dics1= {}
     for _ in range(3):
-        try:  # redis에서 이유 없이 None 이 나오는 경우가 매우 드물게 있어서 3번 시도.
+        try:  # # redis에서 이유 없이 None 이 나오는 경우가 매우 드물게 있어서 3번 시도.
             if gid is None:  # 사용자에게서 gid 가 안옴
                 u_vec = r1.get(ga)
                 if u_vec == None:  # 사용자 ga에 해당하는 벡터가 없음
@@ -54,14 +41,11 @@ async def hello(ga:str, gid:str=None):
             else:  # 사용자에게서 gid 가 옴
                 g_vec = r.get(gid)
                 u_vec = r1.get(ga)
-
                 if u_vec is not None:  # ga에 해당하는 벡터가 있음
                     if g_vec is not None:  # gid에 해당하는 벡터가 있음
                         pass  # 걍 하면 됨
-
                     else:  # gid에 해당하는 벡터가 없음
                         g_vec = u_vec  # 유저 벡터를 기사 벡터로 씀 (이전에 봤던게 더 강화됨)
-
                 else:  # ga에 해당하는 벡터가 없음. 처음오는 손님
                     if g_vec is not None:  # gid에 해당하는 벡터는 있음
                         u_vec = g_vec
@@ -69,13 +53,13 @@ async def hello(ga:str, gid:str=None):
                         # 완전히 새로운 추천
                         u_vec = r2.get('temp')
                         g_vec = u_vec
-
             u_vec = np.frombuffer(u_vec, dtype='float32')
             g_vec = np.frombuffer(g_vec, dtype='float32')
             u_vec = (1 - p) * g_vec + p * u_vec
             r1.set(ga, u_vec.tobytes())
+            r1.expire(ga, 2592000)#60*60*24*30 : 30일 뒤
             mat = r2.get('mat')
-            mat = np.frombuffer(mat).reshape(700, 50)
+            mat = np.frombuffer(mat).reshape(-1, 50)
             top10 = list(find_10_alt(mat, u_vec))
             a = r2.get('title')
             title_list = json.loads(a)
@@ -87,14 +71,7 @@ async def hello(ga:str, gid:str=None):
             break
         except Exception as e:
             print(e)
-    end = timeit.default_timer()
-    # print(end-start)
-    time1.append(end - start)
-
     return dics1
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8001, host='0.0.0.0')
-    # uvicorn.run(app, port=8001, host='localhost')
-
-    #####
+    uvicorn.run(app, port=80, host='0.0.0.0')
