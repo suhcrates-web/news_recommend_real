1a35bca12a00d801db0d16a7042a677867424b41
suhcrates <suhcrates@hanmail.net>
2023-03-19 00:49:16 +0900

    asdf

--- crawl_30.py
+++ crawl_30.py
@@ -7,7 +7,7 @@ import binascii
 import re
 
 today0 = date.today()
-for i in range(51):
+for i in range(1,51):
     db = mysql.connector.connect(**config)
     cursor = db.cursor()
     print(f"날짜:{i}")
