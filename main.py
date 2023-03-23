6375d9bf5f09939799a4a9a4de308642f350cf79
suhcrates <suhcrates@hanmail.net>
2023-03-23 12:10:59 +0900

    asdf

--- main.py
+++ main.py
@@ -86,5 +86,5 @@ async def hello(ga:str, gid:str=None):
             print(e)
     return dics1
 
-if __name__ == '__main__':
-    uvicorn.run(app, port=8001, host='localhost')
+# if __name__ == '__main__':
+#     uvicorn.run(app, port=8001, host='localhost')
